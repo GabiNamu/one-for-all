2096d43fa61c526450c31d3c88afa9e52d7c047b
Gabriela Muniz <gabrielanamu@live.com>
2023-03-03 20:43:59 -0300

    fix: try to fix req6

--- challenges/desafio6.sql
+++ challenges/desafio6.sql
@@ -1,3 +1,3 @@
-SELECT MIN(p.price) as faturamento_minimo,  MAX(p.price) as faturamento_maximo, ROUND(AVG(p.price), 2) as faturamento_medio, ROUND(SUM(p.price), 2) as faturamento_total
+SELECT FORMAT(MIN(p.price), 2) as faturamento_minimo,  MAX(p.price) as faturamento_maximo, ROUND(AVG(p.price), 2) as faturamento_medio, ROUND(SUM(p.price), 2) as faturamento_total
 FROM SpotifyClone.plans as p
 INNER JOIN SpotifyClone.users as u ON u.plan_id = p.id;

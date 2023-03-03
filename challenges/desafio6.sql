e07e7eaeec7e405ca6289f83078dca291f159e81
Gabriela Muniz <gabrielanamu@live.com>
2023-03-03 20:53:38 -0300

    fix: req6 - change int to a string

--- challenges/desafio6.sql
+++ challenges/desafio6.sql
@@ -1,3 +1,4 @@
-SELECT FORMAT(MIN(p.price), 2) as faturamento_minimo,  MAX(p.price) as faturamento_maximo, ROUND(AVG(p.price), 2) as faturamento_medio, ROUND(SUM(p.price), 2) as faturamento_total
+SELECT FORMAT(MIN(p.price), 2) as faturamento_minimo, CONVERT(MAX(p.price), CHAR) as faturamento_maximo, 
+CONVERT(ROUND(AVG(p.price), 2), CHAR) as faturamento_medio, CONVERT(ROUND(SUM(p.price), 2), CHAR) as faturamento_total
 FROM SpotifyClone.plans as p
 INNER JOIN SpotifyClone.users as u ON u.plan_id = p.id;

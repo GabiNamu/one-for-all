e668808cb43084494c746b83c4402b755e18e249
Gabriela Muniz <gabrielanamu@live.com>
2023-03-03 19:56:08 -0300

    feat: add req6

---
+++ challenges/desafio6.sql
@@ -0,0 +1,4 @@
+SELECT FORMAT(MIN(p.price), 2) as faturamento_minimo,  MAX(p.price) as faturamento_maximo, 
+ROUND(AVG(p.price), 2) as faturamento_medio, ROUND(SUM(p.price), 2) as faturamento_total
+FROM SpotifyClone.plans as p
+INNER JOIN SpotifyClone.users as u ON u.plan_id = p.id;

47d15f81b5749a3429671349b2545788106908df
Gabriela Muniz <gabrielanamu@live.com>
2023-03-04 16:33:09 -0300

    feat: add req13

---
+++ challenges/desafio13.sql
@@ -0,0 +1,9 @@
+SELECT CASE 
+  WHEN u.age <= 30 THEN 'Até 30 anos'
+  WHEN u.age BETWEEN 31 AND 60 THEN 'Entre 31 e 60 anos'
+  WHEN u.age > 60 THEN 'Maior de 60 anos'
+END as faixa_etaria, COUNT(DISTINCT u.age) as total_pessoas_usuarias, COUNT(f.user_id) as total_favoritadas
+FROM SpotifyClone.users AS u
+LEFT JOIN SpotifyClone.favorite_songs as f ON f.user_id = u.id
+GROUP BY faixa_etaria
+ORDER BY faixa_etaria;
